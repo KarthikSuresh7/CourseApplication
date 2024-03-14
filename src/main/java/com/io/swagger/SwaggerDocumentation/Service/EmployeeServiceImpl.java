dd8ed18484f36a281abca49b31c8726598566435
KarthikSuresh7 <skarthikpec@gmail.com>
2024-03-14 23:13:42 +0530

    Feat : SwaggerConfig and two new api's is added

--- src/main/java/com/io/swagger/SwaggerDocumentation/Service/EmployeeServiceImpl.java
+++ src/main/java/com/io/swagger/SwaggerDocumentation/Service/EmployeeServiceImpl.java
@@ -3,10 +3,12 @@ package com.io.swagger.SwaggerDocumentation.Service;
 import com.io.swagger.SwaggerDocumentation.form.EmployeeForm;
 import com.io.swagger.SwaggerDocumentation.model.Employee;
 import com.io.swagger.SwaggerDocumentation.respository.EmployeeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmployeeServiceImpl implements  EmployeeService{
 
     @Autowired
@@ -19,6 +21,7 @@ public class EmployeeServiceImpl implements  EmployeeService{
         e.setEmployeename(eForm.getEmpName());
         e.setJoiningDate(eForm.getJoiningDate());
         employeeRepository.save(e);
+        log.info("Employee saved successfully");
         return e;
     }
 
